1bc578a8ca86c88ec9ebd1bc84d92d26d13623cc
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-27 22:37:26 +0900

    companyコンポーネントを作成

---
+++ src/app/components/company.tsx
@@ -0,0 +1,48 @@
+export default function Company() {
+  return (
+    <div className="bg-orange-500 text-white">
+      <div className="py-3">
+        <img src="" alt="company image" className="" />
+      </div>
+      <p className="border-b border-white mx-3 py-3">
+        会社概要
+      </p>
+      <p className="mx-3 py-2">企業名</p>
+      <p className="border-b border-white mx-3 pb-2">
+        株式会社 サンプルCompany01
+      </p>
+      <p className="mx-3 py-2">住所</p>
+      <p className="mx-3">
+        【本社】
+      </p>
+      <p className="border-b border-white mx-3 pb-2">
+        【工場】
+      </p>
+      <p className="mx-3 py-2">代表者</p>
+      <p className="border-b border-white mx-3 pb-2">
+        サンプル太郎
+      </p>
+      <p className="mx-3 py-2">設立年月日</p>
+      <p className="border-b border-white mx-3 pb-2">
+        19xx年 x月 x日
+      </p>
+      <p className="mx-3 py-2">代表電話番号</p>
+      <p className="border-b border-white mx-3 pb-2">
+        0120-xx-xxxx
+      </p>
+      <p className="mx-3 py-2">業種</p>
+      <p className="mx-3">製造業・その他</p>
+      <p className="mx-3">（主な製品）</p>
+      <p className="border-b border-white mx-3 pb-2">
+        自動車関連部品
+      </p>
+      <p className="mx-3 py-2">法人番号</p>
+      <p className="border-b border-white mx-3 pb-2">
+        1234xxxxxxxxxxxx
+      </p>
+      <div className="flex justify-center py-3">
+        <img src="" alt="Logo" className="" />
+      </div>
+    </div>          
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react"
 import PcMenu from "@/app/components/pc-menu"
 import MobileMenu from "@/app/components/mobile-menu"
+import Company from "@/app/components/company"
 
 export default function Home() {
   const [ isOpen, setIsOpen ] = useState(false)
@@ -39,26 +40,33 @@ export default function Home() {
       </div>
 
       <div>
-        ITと匠の融合
+        <div>
+          ITと匠の融合
+        </div>
+        <div>
+          「金型」は身近にある色々なものの「形」を作るもの。
+        </div>
+        <div>
+          私たちが普段目にする多くのものは、実は「金型」から作り上げられているということをご存知でしょうか？
+        </div>
+        <div>
+          自動車や新幹線、飛行機の部品だけでなく生活に直接関係する身近なものまで、
+        </div>
+        <div>
+          あらゆるものが「金型」を通して作られています。
+        </div>
+        <div>
+        その中でも当社の強みは最新の情報・技術・設備を擁し、創業40年以来培ってきたノウハウや経験を融合し
+        </div>
+        <div>
+          お客様のニーズを捉え、価値ある製品やサービスを提供し続けていることです。    
+        </div>
       </div>
+
       <div>
-        「金型」は身近にある色々なものの「形」を作るもの。
+        <Company />
       </div>
-      <div>
-        私たちが普段目にする多くのものは、実は「金型」から作り上げられているということをご存知でしょうか？
-      </div>
-      <div>
-        自動車や新幹線、飛行機の部品だけでなく生活に直接関係する身近なものまで、
-      </div>
-      <div>
-        あらゆるものが「金型」を通して作られています。
-      </div>
-      <div>
-      その中でも当社の強みは最新の情報・技術・設備を擁し、創業40年以来培ってきたノウハウや経験を融合し
-      </div>
-      <div>
-        お客様のニーズを捉え、価値ある製品やサービスを提供し続けていることです。    
-      </div>
+
     </>
   )
 }
