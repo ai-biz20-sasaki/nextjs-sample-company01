c6226dbf6b54de6cd88742d08de3ce0bd14422b3
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-27 20:58:19 +0900

    フォントNoto_Sans_JPがnpm run buildエラーになるのを修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import './globals.css'
 import { Inter, Noto_Sans_JP } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
-//const noto = Noto_Sans_JP({ weight: ['400'], subsets: ['latin'] });
-const noto = Noto_Sans_JP({ weight: ['400', '700'], subsets: ['latin'], variable: '--font-noto' });
+const noto = Noto_Sans_JP({ weight: ['400', '700'], subsets: ['latin'], preload: false, variable: '--font-noto' });
+//const noto = Noto_Sans_JP({ weight: ['400', '700'], subsets: ['latin'], variable: '--font-noto'});
 
 export const metadata = {
   title: '(株)サンプルカンパニー01',
