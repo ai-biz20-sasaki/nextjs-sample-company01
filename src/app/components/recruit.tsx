96c229a2f714e9a06d14db70b9ba9665980b160b
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-28 15:54:36 +0900

    recruitコンポーネントを改善

--- src/app/components/recruit.tsx
+++ src/app/components/recruit.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image"
+
 export default function Recruit() {
   return (
-    <div>
+    <div className="flex flex-col m-3">
+      <div className="py-3">
+        <Image src="/imgs/title_Recruit.svg" alt="recrut logo" width={300} height={75} />
+      </div>
+      <div className="mb-6">
+        採用情報
+      </div>
       <div>
         金型はものづくりにはかかせないもの。
       </div>
@@ -10,7 +18,7 @@ export default function Recruit() {
       <div>
         携わってみませんか？
       </div>
-      <div className="flex justify-center py-3">
+      <div className="flex justify-center py-6">
         <button className="bg-lime-500 text-white rounded-full px-10 py-4">
           採用情報はこちら
         </button>
