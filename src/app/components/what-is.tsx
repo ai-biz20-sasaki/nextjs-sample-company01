a493a7a89910af48a9549b4a5644f4820e7776ee
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-28 15:07:43 +0900

    what-isコンポーネントを作成

---
+++ src/app/components/what-is.tsx
@@ -0,0 +1,28 @@
+
+export default function WhatIs() {
+  return (
+    <div className="flex flex-col m-3">
+      <div className="flex justify-center items-center">
+        <span className="text-2xl">IT</span>と<span className="text-2xl">匠</span>の<span className="text-2xl">融合</span>
+      </div>
+      <div className="flex justify-center items-center">
+        <span className="text-2xl">「金型」</span>は身近にある色々なものの<span className="text-2xl">「形」</span>を作るもの。
+      </div>
+      <div className="flex justify-center">
+        私たちが普段目にする多くのものは、実は「金型」から作り上げられているということをご存知でしょうか？
+      </div>
+      <div className="flex justify-center">
+        自動車や新幹線、飛行機の部品だけでなく生活に直接関係する身近なものまで、
+      </div>
+      <div className="flex justify-center">
+        あらゆるものが「金型」を通して作られています。
+      </div>
+      <div className="flex justify-center">
+        その中でも当社の強みは最新の情報・技術・設備を擁し、創業40年以来培ってきたノウハウや経験を融合し
+      </div>
+      <div className="flex justify-center">
+        お客様のニーズを捉え、価値ある製品やサービスを提供し続けていることです。    
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react"
 import PcMenu from "@/app/components/pc-menu"
 import MobileMenu from "@/app/components/mobile-menu"
+import WhatIs from "@/app/components/what-is"
 import News from "@/app/components/news"
 import Company from "@/app/components/company"
 import Recruit from "@/app/components/recruit"
@@ -56,27 +57,7 @@ export default function Home() {
       </div>
 
       <div>
-        <div>
-          ITと匠の融合
-        </div>
-        <div>
-          「金型」は身近にある色々なものの「形」を作るもの。
-        </div>
-        <div>
-          私たちが普段目にする多くのものは、実は「金型」から作り上げられているということをご存知でしょうか？
-        </div>
-        <div>
-          自動車や新幹線、飛行機の部品だけでなく生活に直接関係する身近なものまで、
-        </div>
-        <div>
-          あらゆるものが「金型」を通して作られています。
-        </div>
-        <div>
-        その中でも当社の強みは最新の情報・技術・設備を擁し、創業40年以来培ってきたノウハウや経験を融合し
-        </div>
-        <div>
-          お客様のニーズを捉え、価値ある製品やサービスを提供し続けていることです。    
-        </div>
+        <WhatIs />
       </div>
 
       <div>
