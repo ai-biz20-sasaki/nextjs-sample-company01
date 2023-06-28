896794d187c8e15d9267bb2f7d2a0dd003e43416
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-29 07:55:59 +0900

    headerに会社ロゴを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 import { useState } from "react"
+import Image from "next/image"
+
 import PcMenu from "@/app/components/pc-menu"
 import MobileMenu from "@/app/components/mobile-menu"
 import WhatIs from "@/app/components/what-is"
@@ -38,7 +40,13 @@ export default function Home() {
       <div className="bg-gray-200/75 sticky top-0 p-3">
         <header className="container mx-auto items-center">
           <div className="flex justify-between">
-            <h1 className="text-lg font-semibold">サンプルCompany01</h1>
+            <div className="flex flex-row items-center">
+              <div className="py-3">
+                <Image src="/imgs/company.svg" alt="news logo" width={35} height={35} />
+              </div>
+              <h1 className="text-lg font-semibold">サンプルCompany01</h1>
+            </div>
+             {/*ハンバーガーメニュー */}
             <button onClick={handleClick}>
               <svg className="md:hidden h-6 w-6 fill-current" viewBox="0 0 24 24">
                 <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
