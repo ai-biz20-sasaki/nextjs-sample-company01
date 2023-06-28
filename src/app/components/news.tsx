71a4df77aca248ff08b14d189cf6e8cfbeaab2ac
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-28 21:20:02 +0900

    newsコンポーネントをレスポンシブ化

--- src/app/components/news.tsx
+++ src/app/components/news.tsx
@@ -2,33 +2,39 @@ import Image from "next/image"
 
 export default function News() {
   return (
-    <div className="m-3">
-      <div className="py-3">
-        <Image src="/imgs/title_News.svg" alt="news logo" width={300} height={75} />
-      </div>
-      <p className="border-b border-black mx-3 py-3">
-        お知らせ
-      </p>
-      <p className="text-lime-500 mx-3 py-2">20xx.04.xx</p>
-      <p className="border-b border-black mx-3 pb-2">
-        GW営業のお知らせ
-      </p>
-      <p className="text-lime-500 mx-3 py-2">20xx.12.xx</p>
-      <p className="border-b border-black mx-3 pb-2">
-        年末年始のお知らせ
-      </p>
-      <p className="text-lime-500 mx-3 py-2">20xx.xx.xx</p>
-      <p className="border-b border-black mx-3 pb-2">
-        溶接機を導入しました。
-      </p>
-      <div className="flex justify-end items-center py-3">
-        <div className="text-white bg-lime-500 rounded-full h-12 w-12 flex justify-center items-center">
-          &gt;
+    <div className="md:flex md:flex-row m-3">
+      <div>
+        <div className="py-3">
+          <Image src="/imgs/title_News.svg" alt="news logo" width={300} height={75} />
         </div>
-        <div className="ml-2">
-          <a href="#">一覧を見る</a>
+        <p className="mx-3 py-3">
+          お知らせ
+        </p>
+      </div>
+
+      <div className="md:flex-auto">
+        <p className="text-lime-500 border-t border-black mx-3 py-2">20xx.04.xx</p>
+        <p className="border-b border-black mx-3 pb-2">
+          GW営業のお知らせ
+        </p>
+        <p className="text-lime-500 mx-3 py-2">20xx.12.xx</p>
+        <p className="border-b border-black mx-3 pb-2">
+          年末年始のお知らせ
+        </p>
+        <p className="text-lime-500 mx-3 py-2">20xx.xx.xx</p>
+        <p className="border-b border-black mx-3 pb-2">
+          溶接機を導入しました。
+        </p>
+        <div className="flex justify-end items-center py-3">
+          <div className="text-white bg-lime-500 rounded-full h-12 w-12 flex justify-center items-center">
+            &gt;
+          </div>
+          <div className="ml-2">
+            <a href="#">一覧を見る</a>
+          </div>
         </div>
       </div>
+
     </div>          
   )
 }
