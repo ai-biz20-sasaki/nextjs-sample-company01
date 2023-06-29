0563736224ae9cbc7dc3b6ade9f5c354ddba3a12
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-29 15:32:13 +0900

    contact-mobile-menuコンポーネントを改善

--- src/app/components/contact-mobile-menu.tsx
+++ src/app/components/contact-mobile-menu.tsx
@@ -3,32 +3,40 @@ import Image from "next/image"
 export default function ContactMobileMenu() {
   return (
     <>
-      <div className="border-4 border-orange-500 mx-16 p-2 rounded-3xl">
-        <div className="flex justify-center text-orange-500">
-          電話でのお問い合わせ
-        </div>
-        <div className="flex justify-center text-4xl text-orange-500">
-          0120-xx-xxxx
-        </div>
-        <div className="flex justify-center text-orange-500">
-          営業時間／8:30-17:30
-        </div>
-      </div>
 
-      <div className="flex flex-row justify-center items-center border-4 border-lime-400 my-4 mx-16 p-4 rounded-3xl">
-        <div className="py-2 mr-3">
-          <Image src="/imgs/mail.svg" alt="mail icon" width={50} height={50} />
-        </div>
-        <div>
-          <div className="text-lime-400">
-            メールでの
+      <div className="container mx-auto flex flex-col items-center md:flex-row md:space-x-8">
+
+        <div className="border-4 border-orange-500 text-orange-500 rounded-3xl w-3/4 p-4 md:basis-1/2 md:h-32">
+          <div className="flex justify-center">
+            電話でのお問い合わせ
           </div>
-          <div className="text-lime-400">
-            お問い合わせ
+          <div className="flex justify-center text-4xl">
+            0120-xx-xxxx
+          </div>
+          <div className="flex justify-center">
+            営業時間／8:30-17:30
           </div>
         </div>
-        <div className="py-2">
-          <Image src="/imgs/arrow_right.svg" alt="→" width={50} height={50} />
+
+        <div className="md:hidden m-3"></div>
+
+        <div className="flex flex-row justify-center items-center border-4 border-lime-400 rounded-3xl w-3/4 p-6 md:basis-1/2 md:h-32">
+          <div className="mr-3">
+            <Image src="/imgs/mail.svg" alt="mail icon" width={50} height={50} />
+          </div>
+          <div className="">
+            <a href="mailto:sample01@example.com">
+            <div className="text-lime-400">
+              メールでの
+            </div>
+            <div className="text-lime-400">
+              お問い合わせ
+            </div>
+            </a>
+          </div>
+          <div className="">
+            <Image src="/imgs/arrow_right.svg" alt="→" width={50} height={50} />
+          </div>
         </div>
       </div>
 
